40f407d1eaf148415de1bfc5af55420f84861fee
Brandon Brooks <brandonbrbrooks@gmail.com>
2024-07-08 18:17:11 +0100

    Added files

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const navbarDropdownLink = document.querySelector('#dropbtn')
 const navbarDropdownContent = document.querySelector(".dropdown-content")
 
-console.log(navbarDropdownLink)
-console.log(navbarDropdownContent)
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
 
 navbarDropdownLink.addEventListener("click", function (e) {
     navbarDropdownContent.classList.toggle("show")
     console.log("Hello World")
 })
-const hamMenu = document.querySelector('.ham-menu');
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 })
+
+// keep track of previous scroll position
+let prevScrollPos = window.pageXOffset;
+
+window.addEventListener('scroll', function() {
+  // current scroll position
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    // user has scrolled up
+    document.querySelector('.main-container').classList.add('show');
+  } else {
+    // user has scrolled down
+    document.querySelector('.main-container').classList.remove('show');
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
